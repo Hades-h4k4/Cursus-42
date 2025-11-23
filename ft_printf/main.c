57fe3c63860b1a0ddc966453503fa4bea3e24500
Hades-h4k4 <carloseldeliver04@gmail.com>
2025-11-23 19:37:44 +0100

    more visual tests

--- ft_printf/main.c
+++ ft_printf/main.c
@@ -6,7 +6,7 @@
 /*   By: carltruj <carltruj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/23 18:34:05 by carltruj          #+#    #+#             */
-/*   Updated: 2025/11/23 19:30:32 by carltruj         ###   ########.fr       */
+/*   Updated: 2025/11/23 19:37:11 by carltruj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 int main()
 {
-	ft_printf("char[%%c]: %c\nstring[%%s]: %s\nnumber[%%d]: %d\n", 'A', "string", 12345);
-	int a = ft_printf("%c%s%d", 'A', "BC", 123);
-	ft_printf("long of a: %d", a);
+	ft_printf("char[%%c]: %c\nstring[%%s]: %s\nnumber[%%d]: %d\nintergers[%%i]: %i\n\n", 'A', "string", 12345, 12345);
+	int a = ft_printf("%c%s%d%i\n", 'A', "BC", 123, 123);
+	ft_printf("long of a: %d\n", a);
 	return (0);
 }
