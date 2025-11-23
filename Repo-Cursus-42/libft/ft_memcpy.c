1e0a0f946b710a2e462f6f87ed4eb6d7e737d853
Hades-h4k4 <carloseldeliver04@gmail.com>
2025-11-23 17:57:33 +0100

    All Done 100% in francinette strict and all OK! in norminette.

---
+++ Repo-Cursus-42/libft/ft_memcpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: carltruj <carltruj@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/10 13:16:55 by carltruj          #+#    #+#             */
+/*   Updated: 2025/11/17 18:02:24 by carltruj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t			i;
+	unsigned char	*s;
+	unsigned char	*d;
+
+	if (!dest && !src)
+		return (NULL);
+	s = (unsigned char *)src;
+	d = (unsigned char *)dest;
+	i = 0;
+	while (i < n)
+	{
+		d[i] = s[i];
+		i++;
+	}
+	return (dest);
+}
---
+++ Repo-Cursus-42/libft/ft_strtrim.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: carltruj <carltruj@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/12 15:46:52 by carltruj          #+#    #+#             */
+/*   Updated: 2025/11/17 18:02:55 by carltruj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	s;
+	size_t	e;
+
+	if (!s1 || !set)
+		return (NULL);
+	s = 0;
+	while (s1[s] && ft_strchr(set, s1[s]))
+		s++;
+	e = ft_strlen(s1);
+	while (e > s && ft_strchr(set, s1[e - 1]))
+		e--;
+	return (ft_substr(s1, s, e - s));
+}
